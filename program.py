2e9721c4b8d9856e79b463657d0dd3d050341d30
lbging <73248866+lbging@users.noreply.github.com>
2023-03-24 22:35:11 -0300

    Update program.py

--- program.py
+++ program.py
@@ -1,16 +1,31 @@
 from modules import actions
 from modules import user
+from modules import sql
 
-print("""
-Available actions:
-    -Register
-    -Login
-""")
 
+def main_menu():
+    print("""
+    Available actions:
+        -Register
+        -Login
+        -Exit
+    """)
+
+    return input("Welcome! What do you wish to do? ").lower()
+
+
+sql.create_tables()
 do = actions.Actions()
-func = input("Welcome! What do you wish to do? ")
 
-if func.lower() == "register":
-    do.register()
-elif func.lower() == "login":
-    do.login()
+while True:
+    func = main_menu()
+
+    if func == "register":
+        do.register()
+    elif func == "login":
+        do.login()
+    elif func == "exit":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option. Please try again.")
