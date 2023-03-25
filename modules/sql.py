a37890822d86d9fdad9b443ffbb88af347ef893f
lbging <73248866+lbging@users.noreply.github.com>
2023-03-24 22:33:33 -0300

    Update sql.py

--- modules/sql.py
+++ modules/sql.py
@@ -1,19 +1,40 @@
-import mysql
-import mysql.connector
 import sqlite3
 
 # SQL connection module
 
-def connectDb():
-    database = mysql.connector.connect(
-        host = "localhost",
-        port = 3306,
-        database = "Python1",
-        user = "root",
-        passwd = "HalfLife3"
-    )
 
-    cursor = database.cursor(buffered=True)
+def connectDb():
+    database = sqlite3.connect("Python1.db")
+    cursor = database.cursor()
 
     return [database, cursor]
 
+
+def create_tables():
+    database, cursor = connectDb()
+
+    create_users_table = '''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        lastname TEXT NOT NULL,
+        mail TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        date_registered TEXT NOT NULL
+    );
+    '''
+
+    create_notes_table = '''
+    CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        date_created TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    );
+    '''
+
+    cursor.execute(create_users_table)
+    cursor.execute(create_notes_table)
+    database.commit()
