4faea5dc11ac0a96fdfd50e932bdd059a912eef5
lbging <73248866+lbging@users.noreply.github.com>
2023-03-24 22:33:48 -0300

    Update user.py

--- modules/user.py
+++ modules/user.py
@@ -1,11 +1,21 @@
 from modules import sql
+import sqlite3
 import datetime
 import hashlib
 
+
 # Database connection function
-connect = sql.connectDb()
+def connectDb():
+    database = sqlite3.connect("Python1.db")
+    cursor = database.cursor()
+
+    return [database, cursor]
+
+
+connect = connectDb()
 database = connect[0]
 cursor = connect[1]
+
 
 class user:
 
@@ -14,7 +24,7 @@ class user:
         self.lastname = lastname
         self.mail = mail
         self.password = password
-    
+
     def register(self):
         date = datetime.datetime.now()
 
@@ -22,22 +32,23 @@ class user:
         encryption = hashlib.sha256()
         encryption.update(self.password.encode('utf8'))
 
-        sql = "INSERT INTO users VALUES(null, %s, %s, %s, %s, %s)"
+        sql = "INSERT INTO users VALUES(null, ?, ?, ?, ?, ?)"
         user = (self.name, self.lastname, self.mail, encryption.hexdigest(), date)
 
         try:
             cursor.execute(sql, user)
             database.commit()
             result = [cursor.rowcount, self]
-        except:
+        except Exception as e:
+            print(f"Error: {e}")
             result = [0, self]
-        
+
         return result
-    
-    def login(self):
+
+    def login(self, cursor, connection):
 
         # User exists?
-        sql = "SELECT * from users WHERE mail = %s AND password = %s"
+        sql = "SELECT * from users WHERE mail = ? AND password = ?"
 
         encryption = hashlib.sha256()
         encryption.update(self.password.encode('utf-8'))
@@ -49,6 +60,3 @@ class user:
         result = cursor.fetchone()
 
         return result
-    
-
-
